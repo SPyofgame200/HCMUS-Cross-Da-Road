4b0807a60adb3ec6582786c62376eda2b6519dcd
Hoang Anh Vo <52689671+SPyofgame200@users.noreply.github.com>
2023-11-21 10:25:17 +0700

    [DEV uMessageManager] optimize code

--- CrossDaRoad-Beta/CrossDaRoad-Beta/uMessageManager.h
+++ CrossDaRoad-Beta/CrossDaRoad-Beta/uMessageManager.h
@@ -16,23 +16,18 @@ public:
     }
 
     void Log(const std::string& functionName, const std::string& message) const {
-        if (TryAcquireToken()) {
-            auto now = std::chrono::steady_clock::now();
-            auto elapsedSinceStart = std::chrono::duration_cast<std::chrono::milliseconds>(now - programStartTime);
+        auto now = std::chrono::steady_clock::now();
+        auto elapsedSinceStart = std::chrono::duration_cast<std::chrono::milliseconds>(now - programStartTime);
 
-            auto minutes = std::chrono::duration_cast<std::chrono::minutes>(elapsedSinceStart);
-            auto seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsedSinceStart - minutes);
-            auto milliseconds = elapsedSinceStart - minutes - seconds;
+        auto minutes = std::chrono::duration_cast<std::chrono::minutes>(elapsedSinceStart);
+        auto seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsedSinceStart - minutes);
+        auto milliseconds = elapsedSinceStart - minutes - seconds;
 
-            std::cerr << "[" << std::setw(2) << std::setfill('0') << minutes.count() << ":"
-                << std::setw(2) << std::setfill('0') << seconds.count() << "."
-                << std::setw(3) << milliseconds.count() << "] ";
-            std::cerr << functionName << "(): " << message << std::endl;
-        }
+        std::cerr << "[" << std::setw(2) << std::setfill('0') << minutes.count() << ":"
+            << std::setw(2) << std::setfill('0') << seconds.count() << "."
+            << std::setw(3) << milliseconds.count() << "] ";
+        std::cerr << functionName << "(): " << message << std::endl;
     }
-
-private:
-    MessageManager() : lastMessageTime(std::chrono::steady_clock::now()), programStartTime(std::chrono::steady_clock::now()) {}
 
     bool TryAcquireToken() const {
         std::lock_guard<std::mutex> lock(tokenMutex);
@@ -47,18 +42,23 @@ private:
         return false;
     }
 
+private:
+    MessageManager() : lastMessageTime(std::chrono::steady_clock::now()), programStartTime(std::chrono::steady_clock::now()) {}
+
     mutable std::mutex tokenMutex;
     mutable std::chrono::steady_clock::time_point lastMessageTime;
     const std::chrono::steady_clock::time_point programStartTime;
-    const int tokenInterval = 100; // 100 milliseconds interval between tokens
+    const int tokenInterval = 500; // 100 milliseconds interval between tokens
 };
 
 // Logging macro
 #define LOG_ERROR(message) \
     do { \
-        std::ostringstream logStream; \
-        logStream << message; \
-        MessageManager::GetInstance().Log(__func__, logStream.str()); \
+        if (MessageManager::GetInstance().TryAcquireToken()) { \
+            std::ostringstream logStream; \
+            logStream << message; \
+            MessageManager::GetInstance().Log(__func__, logStream.str()); \
+        } \
     } while(0)
 
 #endif // U_MESSAGE_MANAGER
