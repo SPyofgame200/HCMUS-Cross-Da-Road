7a6fa9409081912278cfa1abe24b2e9e34c6c013
Hoang Anh Vo <52689671+SPyofgame200@users.noreply.github.com>
2023-11-21 18:14:13 +0700

    [DEV uMessageManager] optimize by handling message in another thread

--- CrossDaRoad-Beta/CrossDaRoad-Beta/uMessageManager.h
+++ CrossDaRoad-Beta/CrossDaRoad-Beta/uMessageManager.h
@@ -7,6 +7,7 @@
 #include <thread>
 #include <mutex>
 #include <iomanip>
+#include <future>
 
 class MessageManager {
 public:
@@ -16,17 +17,21 @@ public:
     }
 
     void Log(const std::string& functionName, const std::string& message) const {
-        auto now = std::chrono::steady_clock::now();
-        auto elapsedSinceStart = std::chrono::duration_cast<std::chrono::milliseconds>(now - programStartTime);
+        auto logTask = std::async(std::launch::async, [=] {
+            std::lock_guard<std::mutex> lock(logMutex);
 
-        auto minutes = std::chrono::duration_cast<std::chrono::minutes>(elapsedSinceStart);
-        auto seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsedSinceStart - minutes);
-        auto milliseconds = elapsedSinceStart - minutes - seconds;
+            auto now = std::chrono::steady_clock::now();
+            auto elapsedSinceStart = std::chrono::duration_cast<std::chrono::milliseconds>(now - programStartTime);
 
-        std::cerr << "[" << std::setw(2) << std::setfill('0') << minutes.count() << ":"
-            << std::setw(2) << std::setfill('0') << seconds.count() << "."
-            << std::setw(3) << milliseconds.count() << "] ";
-        std::cerr << functionName << "(): " << message << std::endl;
+            auto minutes = std::chrono::duration_cast<std::chrono::minutes>(elapsedSinceStart);
+            auto seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsedSinceStart - minutes);
+            auto milliseconds = elapsedSinceStart - minutes - seconds;
+
+            std::cerr << "[" << std::setw(2) << std::setfill('0') << minutes.count() << ":"
+                << std::setw(2) << std::setfill('0') << seconds.count() << "."
+                << std::setw(3) << milliseconds.count() << "] ";
+            std::cerr << functionName << "(): " << message << std::endl;
+            });
     }
 
     bool TryAcquireToken() const {
@@ -46,6 +51,7 @@ private:
     MessageManager() : lastMessageTime(std::chrono::steady_clock::now()), programStartTime(std::chrono::steady_clock::now()) {}
 
     mutable std::mutex tokenMutex;
+    mutable std::mutex logMutex;  // Mutex to synchronize access to logging
     mutable std::chrono::steady_clock::time_point lastMessageTime;
     const std::chrono::steady_clock::time_point programStartTime;
     const int tokenInterval = 500; // 100 milliseconds interval between tokens
