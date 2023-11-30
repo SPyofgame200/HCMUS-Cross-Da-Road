298ea95b0be624f427a26a94da346a85942192f3
wasdqaz <mrkhoiphamanh@gmail.com>
2023-11-30 14:18:35 +0700

    qwe

--- CrossDaRoad-Beta/CrossDaRoad-Beta/cApp.cpp
+++ CrossDaRoad-Beta/CrossDaRoad-Beta/cApp.cpp
@@ -417,42 +417,43 @@ std::string cApp::GetFilePathLocation(bool isSave, std::string fileName)
 
 /// @brief Save current game state to file
 /// @return True if game is saved successfully, false otherwise
-bool cApp::OnGameSave() const
-{
-	const std::string sSaveFilePath = Menu.GetFileLocation() + "/" + playerName + ".bin";
-	if (!sSaveFilePath.empty()) {
-		std::ofstream fout(sSaveFilePath, std::ios::binary);
-		if (fout.is_open()) {
-			// Writing data to the binary file
-			int mapLevel = MapLoader.GetMapLevel();
-			float playerVelocityX = Player.Physic().GetPlayerVelocityX();
-			float playerVelocityY = Player.Physic().GetPlayerVelocityY();
-			float playerAnimationPosX = Player.Physic().GetPlayerAnimationPositionX();
-			float playerAnimationPosY = Player.Physic().GetPlayerAnimationPositionY();
-			float playerLogicPosX = Player.Physic().GetPlayerLogicPositionX();
-			float playerLogicPosY = Player.Physic().GetPlayerLogicPositionY();
+bool cApp::OnGameSave() const {
+    const std::string sSaveFilePath = Menu.GetFileLocation() + "/" + playerName + ".bin";
+    if (!sSaveFilePath.empty()) {
+        std::ofstream fout(sSaveFilePath, std::ios::binary);
+        if (fout.is_open()) {
+            // Writing data to the binary file
+            int mapLevel = MapLoader.GetMapLevel();
+            float playerVelocityX = Player.Physic().GetPlayerVelocityX();
+            float playerVelocityY = Player.Physic().GetPlayerVelocityY();
+            float playerAnimationPosX = Player.Physic().GetPlayerAnimationPositionX();
+            float playerAnimationPosY = Player.Physic().GetPlayerAnimationPositionY();
+            float playerLogicPosX = Player.Physic().GetPlayerLogicPositionX();
+            float playerLogicPosY = Player.Physic().GetPlayerLogicPositionY();
 
-			fout.write(reinterpret_cast<const char*>(&mapLevel), sizeof(int));
-			fout.write(reinterpret_cast<const char*>(&playerVelocityX), sizeof(float));
-			fout.write(reinterpret_cast<const char*>(&playerVelocityY), sizeof(float));
-			fout.write(reinterpret_cast<const char*>(&playerAnimationPosX), sizeof(float));
-			fout.write(reinterpret_cast<const char*>(&playerAnimationPosY), sizeof(float));
-			fout.write(reinterpret_cast<const char*>(&playerLogicPosX), sizeof(float));
-			fout.write(reinterpret_cast<const char*>(&playerLogicPosY), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&mapLevel), sizeof(int));
+            fout.write(reinterpret_cast<const char*>(&playerVelocityX), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&playerVelocityY), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&playerAnimationPosX), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&playerAnimationPosY), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&playerLogicPosX), sizeof(float));
+            fout.write(reinterpret_cast<const char*>(&playerLogicPosY), sizeof(float));
 
-			fout.close();
-			return true;
-		}
-		else {
-			std::cerr << "Can't open the selected file for writing." << std::endl;
-			return false;
-		}
-	}
-	else {
-		std::cerr << "User canceled the save operation." << std::endl;
-		return false;
-	}
+            fout.close();
+            std::cout << "Game data saved successfully." << std::endl;
+            return true;
+        }
+        else {
+            std::cerr << "Error: Can't open the selected file for writing." << std::endl;
+            return false;
+        }
+    }
+    else {
+        std::cerr << "Error: Invalid file path or user canceled the save operation." << std::endl;
+        return false;
+    }
 }
+
 /// @brief Load game state from file
 /// @return True if game is loaded successfully, false otherwise
 bool cApp::OnGameLoad()
