b3dfc8fbd0fbad70324001e500bd5408f56d4944
khiemG921 <126588913+khiemG921@users.noreply.github.com>
2023-11-30 14:45:52 +0700

    [map] adjust map 5

--- CrossDaRoad-Beta/CrossDaRoad-Beta/cAssetManager.cpp
+++ CrossDaRoad-Beta/CrossDaRoad-Beta/cAssetManager.cpp
@@ -381,6 +381,7 @@ bool cAssetManager::LoadMapOceanSprites()
     bSuccess &= LoadSprite("deep_ocean", "deep_ocean");
     bSuccess &= LoadAnimation("octopus", "octopus", 6);
     bSuccess &= LoadAnimation("shark", "shark", 6);
+    bSuccess &= LoadAnimation("shark_left", "shark_left", 6);
 
     return ReportLoadingResult(bSuccess, "Ocean map");
 }
